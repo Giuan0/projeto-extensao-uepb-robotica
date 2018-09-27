428de638d62d0b8a62baef03f407fb690560aab8
Giuan Adauto <giuanadauto@hotmail.com>
2018-09-27 13:59:21 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+var five = require("johnny-five");
+var board = new five.Board();
+const app = require('express')();
+
+const cors = require('cors')
+app.use(cors());
+
+board.on("ready", ()=>{//quando a placa estiver pronta, então...
+
+
+  const led = new five.Led(8);
+
+  app.get('/ligar', (req, res)=>{
+    led.on();
+    return res.json({data: 'ligou'});
+  })
+
+  app.get('/desligar', (req, res)=>{
+    led.off();
+    return res.json({data: 'desligou'});
+  })
+
+
+});
+
+app.listen(3000, ()=>{//põe o servidor no ar
+  console.log('Linstenning on port 3000');
+})
